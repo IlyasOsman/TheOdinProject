5c6594378d33c12d6853c2d073cf9a0dbc612690
ilyasosman <earlyosman@gmail.com>
2023-01-25 18:53:15 +0300

    complete

--- Ruby/caesar_cipher.rb
+++ Ruby/caesar_cipher.rb
@@ -20,14 +20,39 @@
 def caesar_cipher(string, shift_number)
 
     # Splitting string into characters then mapping over each character to change into ascii 
-    asciis = string.Split("").map{ |c| c.ord }
+    asciis = string.split("").map{ |c| c.ord }
 
-    asciis.map |ascii| do 
+    shifted = asciis.map do |ascii|
         # checking if  asci is within alphabetic ranges
         if ascii.between?(65, 90)
             # condition if it goes out of ranges
             if shift_number.positive?
+                (ascii + shift_number) > 90 ? (ascii + shift_number) - 26 : (ascii + shift_number)
+            else shift_number.negative?
+                (ascii - shift_number) < 65 ? (ascii - shift_number) + 26 : (ascii - shift_number)
+            end
+        elsif ascii.between?(97, 122)
+            if shift_number.negative?
+                (ascii + shift_number) > 122 ? (ascii + shift_number) - 26 : (ascii + shift_number)
+            else shift_number.negative?
+                (ascii - shift_number) < 97 ? (ascii - shift_number) + 26 : (ascii - shift_number)
+            end
+        else
+            ascii
         end
     end
 
+    shifted_string = shifted.map! { |c| c.chr }.join
+
+    puts shifted_string
+
 end
+
+
+# print "Enter the string to encode : "
+# string = gets.chomp
+
+# print "Enter the shift factor for the string : "
+# shift_factor = gets.chomp.to_i
+
+# caesar_cipher(string,shift_factor)
